6994feaea3ce00145cc7ed218d244ccc0a12ab3c
Joseph Loic Bisso-Bi-Ela <j.bisso-bi-ela@tu-braunschweig.de>
2021-02-10 14:16:58 +0100

    Klasse MyGame erstellt

---
+++ src/main/java/student/MyGame.java
@@ -0,0 +1,12 @@
+package student;
+
+// Erlaubte Pakete: java.lang, java.io, java.nio, java.util und org.junit;
+
+import ias.Deck;
+import ias.Factory;
+import ias.Game;
+import ias.GameException;
+
+public class MyGame implements Game, Factory, Deck {
+	
+}
